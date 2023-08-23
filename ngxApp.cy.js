685e70578a328ae4bca6a418a40a8e28a142716d
Ivan Hristov <128089859+Vacheto1986@users.noreply.github.com>
2023-08-23 17:40:07 +0300

    Added ngxApp.cy.js
    Added a few tests

--- ngxApp.cy.js
+++ ngxApp.cy.js
@@ -2,9 +2,13 @@
 
 describe('ngx platform', () => {
 
-    it('verify "Using the Grid" form', () => {
-
+    beforeEach(() => {
         cy.visit('/')
+    })
+
+    it.skip('verify "Using the Grid" form', () => {
+
+        
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
@@ -18,9 +22,9 @@ describe('ngx platform', () => {
 
     })
 
-    it.only('verify "Basic form" form', () => {
+    it.skip('verify "Basic form" form', () => {
 
-        cy.visit('/')
+        
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
@@ -38,8 +42,46 @@ describe('ngx platform', () => {
             const passLabelFirst = basicForm.find('[for="exampleInputPassword1"]').text()
             expect(emailLabelFirst).to.equal('Email address')
             expect(passLabelFirst).to.equal('Password')
+        })
+
+    })
+
+    it('Theme color change button', () => {
+
+        // cy.get('nav nb-select').click()
+        // cy.get('.options-list')
+        //     .contains('Dark')
+        //     .click()
+        // cy.get('nav nb-select')
+        //     .should('contain', 'Dark')
+        // cy.get('nb-layout-header nav')
+        //     .should('have.css', 'background-color', 'rgb(34, 43, 69)')
+        
+        cy.get('nav nb-select').then( dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list')
+            cy.get('nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+
+                if(index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+
+            })
 
         })
+
     })
 
 })
